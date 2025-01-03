87102e1af26d0c2265db2825e29af2ddc92abe08
thehassanbilal <hassanbilalchannel@gmail.com>
2025-01-03 17:55:05 +0400

    feat: add request logging middleware to main.ts for improved monitoring of incoming requests and response durations

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -6,6 +6,21 @@ import { ArrayResponseInterceptor } from './common/interceptors/array-response.i
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.use((req, res, next) => {
+    console.log(
+      `[${new Date().toISOString()}] Incoming ${req.method} request to ${req.url}`,
+    );
+
+    const start = Date.now();
+    res.on('finish', () => {
+      const duration = Date.now() - start;
+      console.log(
+        `[${new Date().toISOString()}] Response sent for ${req.method} ${req.url} - Status: ${res.statusCode} - Duration: ${duration}ms`,
+      );
+    });
+
+    next();
+  });
   app.enableCors();
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new ArrayResponseInterceptor());
